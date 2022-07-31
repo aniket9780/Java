106ea293c67fc51b536c91c3b81c1a8cbbac8e0c
Aniket Sharma <aniket.9780@gmail.com>
2022-07-31 14:06:34 +0530

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        bike b = new bike();
+        car c = new car();
+
+        b.run(5);
+        c.type("BMW");
+        c.petrol("Disel");
+    }
+}
